e4962c9385285fd245064724b40c36674212b8ee
beckychow <beckydzau@gmail.com>
2016-01-27 10:34:09 -0800

    search box is now relocated when window size changes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,17 +14,9 @@ MainWindow::MainWindow(QWidget *parent) :
 void MainWindow::resizeEvent(QResizeEvent *event) {
     QMainWindow::resizeEvent(event);
     QWidget *central = centralWidget();
-    QVBoxLayout *layout = central->findChild<QVBoxLayout *>("verticalLayout");
-    //QTextEdit *text = central->findChild<QTextEdit *>("textEdit");
-    //QPushButton *button = central->findChild<QPushButton *>("pushButton");
-    cout << "window " << width() << " " << height() << endl;
-    cout << "central " << central->width() << " " << central->height() << endl;
-    cout << "layout " << layout->geometry().width() << " " << layout->geometry().height() << endl;
-    //layout->geometry().setX((central->width() - layout->geometry().width()) / 2);
-    //layout->geometry().setY((central->height() - layout->geometry().height()) / 2);
-    layout->geometry().moveCenter(QPoint(central->width() / 2), central->height() / 2);
-    //central->move(QPoint((central->width() - layout->geometry().width()) / 2,
-      //                   (central->height() - layout->geometry().height()) / 2));
+    QWidget *layoutContainer = central->findChild<QWidget *>("layoutContainer");
+    layoutContainer->move(QPoint((central->width() - layoutContainer->geometry().width()) / 2,
+                                 (central->height() - layoutContainer->geometry().height()) / 2));
 }
 
 MainWindow::~MainWindow()
