5d4174890102fd329393c3a4f2c17463b1d75984
Huajie Wu <cs100w35@acs-cseb250-05.ucsd.edu>
2016-02-02 20:56:52 -0800

    [Read the Guide] Deployable Version

--- mylineedit.cpp
+++ mylineedit.cpp
@@ -1,40 +1,32 @@
 #include "mylineedit.h"
-#include <QDebug>
 
-MyLineEdit::MyLineEdit(QWidget *parent) : QLineEdit(parent)
-{
+MyLineEdit::MyLineEdit(QWidget *parent) : QLineEdit(parent) {
     wordList = parent->parentWidget()->findChild<WordList *>("listWidget");
-    qDebug() << "wordList pointer: " << wordList << endl;
     connect(this, SIGNAL(textEdited(const QString &)),
             this, SLOT(storeOriginal(const QString &)));
 }
 
 void MyLineEdit::setWordList(WordList * listPtr) {
-    wordList =listPtr;
-    qDebug() << "wordList pointer: " << wordList << endl;
+    wordList = listPtr;
 }
 
 MyLineEdit::~MyLineEdit() {}
 
 void MyLineEdit::storeOriginal() {
     originalString = text();
-    qDebug() << "Original Stored: " << originalString;
 }
 
 void MyLineEdit::storeOriginal(const QString & str) {
     originalString = str;
-    qDebug() << "Original Stored: " << originalString;
 }
 
 void MyLineEdit::keyPressEvent(QKeyEvent *event)
 {
     switch(event->key()) {
         case Qt::Key_Down: // move down highlighter of wordList
-            qDebug() << this << "key down pressed!";
             wordList->selectNext();
             break;
         case Qt::Key_Up:   // move up highlighter of wordList
-            qDebug() << this << "key up pressed!";
             wordList->selectPrev();
             break;
         case Qt::Key_Return:
