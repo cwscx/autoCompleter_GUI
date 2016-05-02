dde01791aaae58ea50846c0b20421b381fdc8592
HUAJIE WU <wuhuajie.flyfire2002@gmail.com>
2016-05-02 16:09:05 -0700

    Remove debug msg

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     std::string path = (argc == 2) ? std::string(argv[1]) : std::string("");
-    qDebug() << path ;
     MainWindow w(path, 0);
     w.show();
 
